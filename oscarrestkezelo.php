ff030c073a40857c717cee1fd0eaaf415176e4b3
Szlej09 <ricsigyula6@gmail.com>
2025-03-11 12:19:58 +0100

    oscarrestkezelo.php kesz

---
+++ oscarrestkezelo.php
@@ -0,0 +1,90 @@
+<?php
+include("restkezelo.php");
+include("oscarpdo.php");
+
+class Oscarrestkezelo extends Restkezelo
+{
+    function getAllOscars()
+    {
+        $oscars=new Oscar();
+        $sorAdat=$oscars->getAllOscars();
+        if (empty($sorAdat)) {
+            $statusCode=404;
+            $sorAdat=array('error'=>"No oscars found!");
+        }
+        else{
+            $statusCode=200;
+        }
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        $result["Oscars"]=$sorAdat;
+        $response =$this->encodeJson($result);
+        $file_path="Oscars.json";
+        $this->printfile($response,$file_path);
+        echo $response;
+    }
+    function getOscarsById($m_ID)
+    {
+        $oscars=new Oscar();
+        $sorAdat=$oscars->getOscarsById($m_ID);
+        if (empty($sorAdat)) {
+            $statusCode=404;
+            $sorAdat=array('error'=>'No oscars found by this id!');
+        }
+        else{
+            $statusCode=200;
+        }
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        $result["OscarsById"]=$sorAdat;
+        $response=$this->encodeJson($result);
+        $file_path="OscarsById.json";
+        $this->printfile($response, $file_path);
+        echo $response;
+    }
+    function getOscarsByType($Mt_name)
+    {
+        $oscars=new Oscar();
+        $sorAdat=$oscars->getOscarsByType($Mt_name);
+        if (empty($sorAdat)) {
+            $statusCode=404;
+            $sorAdat=array('error'=>'No oscars found by this type!');
+        }
+        else{
+            $statusCode=200;
+        }
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        $result["OscarsByType"]=$sorAdat;
+        $response=$this->encodeJson($result);
+        $file_path="OscarsByType.json";
+        $this->printfile($response, $file_path);
+        echo $response;
+    }
+    function getFault()
+    {
+        $statusCode=400;
+        $this->sethttpFejlec($statusCode);
+        header('Content-Type: application/json; charset=utf-8');
+        $sorAdat=array('error'=>'Bad Request!');
+        $result["Fault"]=$sorAdat;
+        $response=$this->encodeJson($result);
+        $file_path="Fault.json";
+        $this->printfile($response,$file_path);
+        echo $response;
+    }
+
+    function encodeJson($responseData)
+    {
+        return json_encode($responseData,JSON_PRETTY_PRINT| JSON_UNESCAPED_UNICODE);
+    }
+    function printfile($responseData,$file_path)
+    {
+        file_put_contents($file_path,$responseData);
+    }
+
+
+
+
+
+}
