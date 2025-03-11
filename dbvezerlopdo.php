f5b1a01fa3b9a665ce9d91116910c3c3f31af264
Szlej09 <ricsigyula6@gmail.com>
2025-03-11 11:31:46 +0100

    dbvezerlopdo.php kesz

---
+++ dbvezerlopdo.php
@@ -0,0 +1,47 @@
+<?php
+
+class DBController
+{
+    private $conn=null;
+    private $host="localhost";
+    private $user="root";
+    private $password="";
+    private $dbname="oscar";
+    function __construct()
+    {
+        $this->connectDB();
+        print 'oscarpdo';
+    }
+    function connectDB()
+    {
+        try {
+            $this->conn=new PDO("mysql:host={$this->host}; dbname={$this->dbname}; charset=utf8",$this->user,$this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection failed: ".$e->getMessage());
+        }
+    }
+    function executeSelectQuery($query,$params=[])
+    {
+        try
+        {
+            $stmt=$this->conn->prepare($query);
+            $stmt->execute($params);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e){
+            die("Query failed: ".$e->getMessage());
+        }
+    }
+
+    function closeDB()
+    {
+        $this->conn=null;
+    }
+
+
+
+
+
+
+}
